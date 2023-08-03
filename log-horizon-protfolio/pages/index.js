c9112a3dbe1ea0d72e8b10bdd1a8a07503c99253
Ayushi Parikh <mailayushi15@gmail.com>
2023-08-03 09:44:56 +0530

    Fixing the image on the landing page

--- log-horizon-protfolio/pages/index.js
+++ log-horizon-protfolio/pages/index.js
@@ -2,11 +2,7 @@ import { Navbar, Text, Button, Grid, Image, Avatar } from '@nextui-org/react';
 import styles from '../styles/navbar.css';
 import Link from 'next/link'
 
-const imageStyle = {
-    border: '1px solid #f06852',
-  }
 
-//css={{"paddingLeft":"700px", "paddingTop":"0px"}}
 
 function Home({ NextPage }){
     return (
@@ -38,28 +34,22 @@ function Home({ NextPage }){
             { /* Jumbotron */ }
                 <Grid.Container gap={2} justify='right'>
                     <Grid >
-                        <div className={styles.text}>
-                        <Text weight={"bold"} size={50} css={{"paddingTop":"80px", textAlign:"center"}}> Team Log Horizon 
-                        </Text>
-                        </div>
-                        <div className={styles.textContainer}>
-                        <Text color='$subText' style={{ wordWrap:"break-word", width:"600px", marginTop:"20px", marginLeft:"20px"}}>
+                    <Text weight={"bold"} size={50} css={{"paddingTop":"80px", textAlign:"center"}}> Team Log Horizon </Text>
+                        <Text color='$subText' style={{ wordWrap:"break-word", width:"600px", marginTop:"20px"}}>
                             A team of enthusiastic individuals working towards building this world a better place. This is our portfolio showcasing the power, creativity and xeal of the team.
                             we beleive in giving back to the community that has givenus so much!
                         </Text>
-                        </div>
+
 
                     </Grid>
                     <Grid >
-                        <div className={styles.imageContainer} >
+                        <div>
                             <Image src="team.png"
                                 alt="Default Image"
                                 objectFit='fill'
-                                // height={600}
-                                // width={800}
+                                height={600}
+                                width={800}
                                 css={{"marginLeft": "10px"}}
-
-                                className={styles.image}
                             />
                         </div>
                        
